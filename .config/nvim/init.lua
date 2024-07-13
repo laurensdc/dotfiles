d171b399e3129a192371602e0ddf72905ed13b14
Laurens <laurens.decock@inthepocket.com>
2024-07-13 22:39:08 +0200

    :sparkles: Nvim: Add markdownlint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,7 @@
 
 --[[
 -- TODO: :checkhealth
--- NOTE: Remember <leader>. and remember gc and gcb
+-- NOTE: Remember <leader>.
 --]]
 
 -- Set <space> as the leader key
@@ -302,7 +302,7 @@ require('lazy').setup({
       opleader = {
         line = '<leader>c',
         -- Let's leave <leader>b open for now, probably will just use visual mode and leader cb it right
-        --  block = '<leader>b',
+        block = '<leader>cb',
       },
     },
   },
@@ -355,6 +355,8 @@ require('lazy').setup({
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        -- TODO: Doesn't work because comment.nvim has a <leader>c mapping
+        ['<leader>c'] = { name = '[C]opy / [C]omment', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
@@ -395,20 +397,9 @@ require('lazy').setup({
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
       -- Two important keymaps to use while in Telescope are:
       --  - Insert mode: <c-/>
       --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
@@ -698,6 +689,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'eslint_d',
         'prettierd',
+        'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -732,6 +724,8 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
 
+      -- TODO: Get format_on_save working async
+      --
       -- format_on_save = function(bufnr)
       --   local slow_format_filetypes = {}
       --
@@ -774,6 +768,7 @@ require('lazy').setup({
         -- is found.
         javascript = { 'prettierd', 'eslint_d' },
         typescript = { 'prettierd', 'eslint_d' },
+        markdown = { 'markdownlint' },
       },
     },
   },
