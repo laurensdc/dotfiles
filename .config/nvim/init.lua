ba79565804207bcbdf310fa5e27fa96c38c06a4b
Laurens <laurens.decock@inthepocket.com>
2025-02-22 14:01:48 +0100

    🔥 Nvim: Remove autopairs plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1040,7 +1040,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint', -- Linting with LSP now
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
