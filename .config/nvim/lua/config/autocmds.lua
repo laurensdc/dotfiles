7835c19738d7f7794e813eb34b45c1a99097b5b4
Laurens DC <coding@laurensdc.com>
2025-09-06 20:26:06 +0200

    🔧 Nvim: Swap symbol keybinds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+-- The fkin way to swap these keybinds
+local group = vim.api.nvim_create_augroup("CoredusKLspKeymaps", { clear = true })
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = group,
+  callback = function()
+    local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+    Keys[#Keys + 1] = {
+      "<leader>ss",
+      function()
+        Snacks.picker.lsp_workspace_symbols()
+      end,
+      desc = "LSP Workspace Symbols",
+      has = "documentSymbol",
+    }
+
+    Keys[#Keys + 1] = {
+      "<leader>sS",
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = "LSP Symbols",
+      has = "workspace/symbols",
+    }
+  end,
+})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,17 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Terminal stuff
 vim.keymap.set({ "n" }, "<c-t>", function()
   Snacks.terminal(nil, { cwd = LazyVim.root() })
 end, { desc = "Toggle Terminal (Root Dir)" })
-
 vim.keymap.set("t", "<c-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-
 vim.keymap.set("t", "<c-\\>", "<c-\\><c-n>", { desc = "Escape Terminal mode" })
 
-vim.keymap.set({ "i", "t" }, "<c-bs>", "<c-w>", { desc = "Delete word back" })
-vim.keymap.set({ "i", "t" }, "<m-bs>", "<c-w>", { desc = "Delete word back" })
+-- Ctrl/Option+backspace to delete word
+vim.keymap.set({ "i", "t", "c" }, "<c-bs>", "<c-w>", { desc = "Delete word back" })
+vim.keymap.set({ "i", "t", "c" }, "<m-bs>", "<c-w>", { desc = "Delete word back" })
 
+-- Cmd+V to paste
 vim.keymap.set({ "i", "c" }, "<D-v>", "<C-r>+", { desc = "Paste from system clipboard" })
 vim.keymap.set("n", "<D-v>", "p", { desc = "Paste from system clipboard" })
 vim.keymap.set("t", "<D-v>", '<c-\\><c-n>"+pi', { desc = "Paste from system clipboard" })
+
+-- Disable bufferline H and L to navigate open buffers
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
